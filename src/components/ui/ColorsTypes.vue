a9bd68ad87c0e4f7cdc01ab05c2a285c26be7e9a
Анастасия Брикун <a.brikun@vipcg.ru>
2021-06-10 15:10:39 +0300

    initialized project & basic styles setup

---
+++ src/components/ui/ColorsTypes.vue
@@ -0,0 +1,27 @@
+<template lang="pug">
+.flex-wrapper
+  .colors-list
+    .colors-list__item
+      .colors-list__item--pic
+      .colors-list__item--desc
+        h2.colors-list__item--title Dark Shade 100%
+        p.colors-list__item--text  #1F2041
+  .types-list
+</template>
+
+<script>
+
+export default {
+  name: 'ColorsTypes',
+  data () {
+    return {}
+  }
+}
+</script>
+
+<style lang="scss">
+
+.ui-title-1 {
+    background: #1f2041 75%;
+}    
+</style>
